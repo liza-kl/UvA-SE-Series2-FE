1f1fbd28fc9da330940b905a673f2683582905a4
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-12-03 11:21:47 +0100

    Only allow json as input

--- src/components/FileHandling.tsx
+++ src/components/FileHandling.tsx
@@ -15,7 +15,7 @@ export const FileHandling = ({
   return (
     <>
       {/* @ts-ignore */}
-      <input type="file" onChange={onUpload} />
+      <input type="file" onChange={onUpload} accept=".json,application/json" />
       <Button type="secondary" ghost auto scale={0.7} onClick={onReset}>
         {resetLabel}
       </Button>
