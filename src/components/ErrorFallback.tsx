70576d2352b8fea7f668f6d96218ba555ef40d66
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2023-12-16 10:44:21 +0100

    added note to error fallback

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -1,10 +1,13 @@
-
 export const ErrorFallback = ({ error, resetErrorBoundary }) => {
-    return (
-        <div role="alert">
-          <p>Something went wrong:</p>
-          <pre>{error.message}</pre>
-          <button onClick={resetErrorBoundary}>Try again</button>
-        </div>
-      )
-}
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <b>1. Please try to reload the page firstly</b>
+      <b>2.If that does not work, click the button bellow</b>
+      <button onClick={resetErrorBoundary}>
+        Try again (refresh the page first!)
+      </button>
+    </div>
+  );
+};
